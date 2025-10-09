0c975c9b379e78a96befc24d7609f7de074da894
sahmed9123 <38222638+https-sai@users.noreply.github.com>
2025-10-09 01:24:10 -0400

    deploy motherfucker

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "refreshing-victory-1b93487cfd.strapiapp.com",
-        port: "",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
